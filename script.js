88c7fbe8878030750f4f6147abf1899322bd33cc
tertcoder <82041184+tertcoder@users.noreply.github.com>
2023-02-17 08:07:48 +0200

    making a glowy blob effect

---
+++ script.js
@@ -0,0 +1,22 @@
+const blob = document.getElementById("blob");
+
+document.body.onclick = (event) => {
+  const { clientX, clientY } = event;
+
+  // blob.style.left = `${clientX}px`;
+  // blob.style.top = `${clientY}px`;
+
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 3000, fill: "forwards" }
+  );
+};
+// document.body.addEventListener("pointermove", function (event) {
+// const { clientX, clientY } = event;
+
+// blob.style.left = `${clientX}px`;
+// blob.style.top = `${clientY}px`;
+// });
